fdb402ac8b6ace41556e9f2a8b902a7962f69bbf
Listar <shnap2010@yandex.ru>
2021-04-05 07:23:32 +0300

    user nested data

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,15 +1,21 @@
 from .models import Post
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
+class UserSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    username = serializers.CharField(max_length=100)
+
 class PostsSerializer(serializers.HyperlinkedModelSerializer):
+  author = UserSerializer() 
   class Meta:
-    model = Post 
+    model = Post
     fields = [
-      # 'id',
-      # 'title',
-      # 'text',
-      # 'done',
+      'id',
+      'title',
+      'text',
+      'done',
       'author',
-      # 'created_at',
-      # 'published_date',
+      'created_at',
+      'published_date',
     ]
